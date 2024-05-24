3190cc42459063a8312886e1b9958e8e18ae3030
dlarge-uie <dlarge@illinois.edu>
2024-05-24 10:28:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         Edit <code>src/App.js</code> and save to reload.
-        </p>
+        What is your name?  
+      </p>
         <a
           className="App-link"
           href="https://reactjs.org"
